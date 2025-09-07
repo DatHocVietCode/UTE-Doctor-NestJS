0dfd6d00c7c99cd1a333d1cba5cda7647aa4de08
DatHocVietCode <td13052004@gmail.com>
2025-09-07 22:01:35 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = ['http://localhost:3000', 'https://ute-doctor-fe.vercel.app'];
+   app.enableCors({
+    origin: allowedOrigins,
+    credentials: true, // nếu dùng cookie
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
