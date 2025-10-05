e79a7adf2ca06e2c080a947c828cee7533f9b770
sonhong9824 <nghongson9824@gmail.com>
2025-10-05 16:18:16 +0700

    push profile

---
+++ src/profile/dto/update-profile.dto.ts
@@ -0,0 +1,23 @@
+import { IsDateString, IsOptional, IsString } from 'class-validator';
+
+export class UpdateProfileDto {
+  @IsOptional()
+  @IsString()
+  fullName?: string;
+
+  @IsOptional()
+  @IsString()
+  gender?: string;
+
+  @IsOptional()
+  @IsDateString()
+  dob?: Date;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+}
