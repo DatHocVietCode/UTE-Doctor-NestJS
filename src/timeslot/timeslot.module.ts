5bde3aea185382bb06fcffd1f045be892a2ac1d0
DatHocVietCode <td13052004@gmail.com>
2025-11-03 19:36:08 +0700

    add timeSlotData Seeder

--- src/timeslot/timeslot.module.ts
+++ src/timeslot/timeslot.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
 import { TimeSlotService } from './timeslot.service';
 import { TimeSlotController } from './timelost.controller';
 import { TimeSlotListener } from './timeslot.listenner';
 import { TimeSlotLog, TimeSlotLogSchema } from './schemas/timeslot-log.schema';
 import { TimeSlotData, TimeSlotDataSchema } from './schemas/timeslot-data.schema';
+import { TimeSlotDataSeeder } from './seeders/timeslot-data.seeder';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: TimeSlotLog.name, schema: TimeSlotLogSchema }]),
             MongooseModule.forFeature([{ name: TimeSlotData.name, schema: TimeSlotDataSchema }])],
   exports: [TimeSlotService, TimeSlotListener], // để module khác có thể dùng
-  providers: [TimeSlotService, TimeSlotListener],
+  providers: [TimeSlotService, TimeSlotListener, TimeSlotDataSeeder],
   controllers: [TimeSlotController],
 })
 export class TimeSlotModule {}
